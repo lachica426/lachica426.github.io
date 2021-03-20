33a99ce2a915e5e9494e79f87144de47ee01d387
Nadine Castro <lachica426@gmail.com>
2021-03-20 17:30:13 -0400

    remove unecessary comment

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -6,7 +6,6 @@ class Social extends Component {
     render() {
         return(
             <div className={rootClass}>
-                // eslint-disable-next-line
                 <a href="https://github.com/lachica426" target="_blank"><i className="fab fa-github"></i></a>
                 <a href="https://instagram.com/nadzcastro" target="_blank"><i className="fab fa-instagram"></i></a>
                 <a href="https://ca.linkedin.com/in/nadzcastro" target="_blank"><i className="fab fa-linkedin-in"></i></a>
